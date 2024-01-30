00bd2273bae5972e3dde7fef9264e875a0754529
faizal108 <69467378+faizal108@users.noreply.github.com>
2024-01-30 16:20:58 +0530

    initilize the project

---
+++ src/main/java/com/faizal108/blogvista/BlogvistaApplication.java
@@ -0,0 +1,13 @@
+package com.faizal108.blogvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogvistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogvistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faizal108/blogvista/BlogvistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faizal108.blogvista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogvistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
